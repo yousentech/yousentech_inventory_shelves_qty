dbbf4987577867fd431cb4fcbe0799961979a3fa
rrawann <rawan.sb@yahoo.com>
2025-08-31 16:32:38 +0300

    return get_shelves in button_validate function

--- models/stock_move.py
+++ models/stock_move.py
@@ -163,3 +163,12 @@ class stock_move(models.Model):
     
     
 
+# class StockPicking(models.Model):
+#     _inherit = "stock.picking"
+
+#     def button_validate(self):
+#         res = super().button_validate()
+#         for picking in self.move_ids:
+#             return picking.get_shelves()
+            
+#         return res
